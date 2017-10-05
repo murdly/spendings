71e7e8d3dcc67f98c97a89ce2ea920bb8eabd70f
murdly <arkadiusz.karbowy@gmail.com>
2017-10-05 09:07:10 +0100

    init

---
+++ app/src/main/java/com/akarbowy/spendingsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.akarbowy.spendingsapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
